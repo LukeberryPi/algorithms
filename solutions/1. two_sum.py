32160664e133a8cdcbd8c191e4916ab46418435d
Luke Berry <lukeberrypi@gmail.com>
2023-02-13 15:44:18 -0300

    solve two_sum exercise

---
+++ solutions/1. two_sum.py
@@ -0,0 +1,21 @@
+nums = [2, 3, 5, 7]
+target = 10
+
+# recebemos uma lista números e um número alvo
+# temos que retornar os índices dos dois valores que somados são iguais ao nosso alvo
+# no exemplo, são os valores 3 e 7, de índices 1 e 3
+
+def two_sum(nums, target):
+    map = {}
+
+    for i, n in enumerate(nums):
+        diff = target - n
+
+        if diff in map:
+            return [map[diff], i]
+        
+        map[n] = i
+    
+    return map
+
+print(two_sum(nums, target))
