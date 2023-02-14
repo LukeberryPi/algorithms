c8650e4a8fed25b13725431045947882a5e53dbb
Luke Berry <lukeberrypi@gmail.com>
2023-02-13 23:36:39 -0300

    improve palindrome_number solution

--- solutions/2.palindrome_number.py
+++ solutions/2.palindrome_number.py
@@ -1,16 +1,19 @@
 EXERCISE_LINK = "https://leetcode.com/problems/palindrome-number/"
 
-num = 12121
+num = 101
 
 # recebemos um número
 # retornamos se esse número é palíndromo ou não
-# 123 é palíndromo de 321
+# 123 é um palíndromo de 321
 
-def palindrome_number(n):
-    num_list = [x for x in str(num)]
+def palindrome_number(x):
+    if x < 0 or str(x)[-1] == 0:
+        return False
+    
+    num_list = [ch for ch in str(x)]
     reversed = num_list[::-1]
 
-    return num_list == reversed
-    
+    return reversed == num_list
+
 
 print(palindrome_number(num))
