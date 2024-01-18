b7cb226fcb61735ecd43831069cd4c28b69f42c1
Luke Berry <lukeberrypi@gmail.com>
2024-01-18 02:10:11 -0300

    adjust styles

---
+++ solutions/12.removeDuplicates.js
@@ -0,0 +1,29 @@
+let nums = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 6, 7, 7];
+let nums1 = [1, 1, 2];
+
+function removeDuplicates(nums) {
+  const seen = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let count = nums.reduce((acc, cur) => {
+      if (cur === nums[i]) {
+        acc++;
+      }
+
+      return acc;
+    }, 0);
+
+    if (seen.has(nums[i])) {
+      nums.splice(i, count - 1);
+    }
+    seen.set(nums[i], true);
+  }
+
+  return nums;
+}
+
+removeDuplicates(nums);
+removeDuplicates(nums1);
+
+console.log(nums);
+console.log(nums1);
