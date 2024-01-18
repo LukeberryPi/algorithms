4db6b57bd0c03072c0c0ca22d20600474339ee96
Luke Berry <lukeberrypi@gmail.com>
2024-01-18 02:48:29 -0300

    adjust styles

---
+++ solutions/13.removeElement.js
@@ -0,0 +1,16 @@
+let nums = [1, 2, 6, 6, 6, 6, 3, 4, 5, 6];
+let val = 6;
+
+// why iterate backwards? https://chat.openai.com/share/2a596495-d1d0-43bb-9cec-b55213db9a9b
+function removeElement(nums, val) {
+  for (let i = nums.length; i >= 0; i--) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    }
+  }
+
+  return nums.length;
+}
+
+console.log('removeElement(nums, val)', removeElement(nums, val))
+console.log('nums', nums)
