a331741e5693c325d5baa687ebcaf15b7284f4c1
Luke Berry <lukeberrypi@gmail.com>
2024-01-21 01:30:09 -0300

    instapush

--- solutions/16.searchInsert.js
+++ solutions/16.searchInsert.js
@@ -1,7 +1,17 @@
-// https://leetcode.com/problems/search-insert-position/description/
+// https://leetcode.com/problems/search-insert-position/description
 
-function searchInsert() {
-  return null;
+const nums = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+const target = 8;
+
+function binarySearch(nums, target) {
+  const len = nums.length;
+  const mid = nums[Math.floor(len / 2)];
+
+  while (mid !== target) {}
+
+  return mid;
 }
 
-console.log('searchInsert()', searchInsert())
+console.log("binarySearch(nums, target)", binarySearch(nums, target));
