69d6bbf61c37a4f1ce14a2332e8eddf0bc14d798
Luke Berry <lukeberrypi@gmail.com>
2024-01-30 02:36:21 -0300

    instapush

---
+++ leetcode/367-valid-perfect-square.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/valid-perfect-square/description/
+
+const num = 16;
+
+function isPerfectSquare(num) {
+  const isSquare = false;
+  return num;
+}
+
+console.log(isPerfectSquare(num));
