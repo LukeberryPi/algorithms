aebe5704b7a7bf247f0fceeba5877ad4a3eb41da
Luke Berry <lukeberrypi@gmail.com>
2024-01-30 23:31:20 -0300

    instapush

--- leetcode/412-fizz-buzz.js
+++ leetcode/412-fizz-buzz.js
@@ -2,22 +2,22 @@
 
 const input = 15;
 
-  function fizzBuzz(n) {
-    let result = [];
+function fizzBuzz(n) {
+  let result = [];
 
-    for (let i = 1; i <= n; i++) {
-      if (i % 5 === 0 && i % 3 === 0) {
-        result.push("FizzBuzz");
-      } else if (i % 5 === 0) {
-        result.push("Buzz");
-      } else if (i % 3 === 0) {
-        result.push("Fizz");
-      } else {
-        result.push(String(i));
-      }
+  for (let i = 1; i <= n; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else {
+      result.push(String(i));
     }
-
-    return result;
   }
+
+  return result;
+}
 
 console.log(fizzBuzz(input));
